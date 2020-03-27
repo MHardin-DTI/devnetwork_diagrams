c20a33fc9a18e31fc70a319cb783b204465c8507
mhardin <melinda.hardin@jocogov.org>
2020-03-27 10:32:31 -0500

    button shows names

--- logic.js
+++ logic.js
@@ -1,15 +1,26 @@
 $(function() {
 
-    const queryURL= "http://mvc.ad.jocoks.com/OrgDataMVC/DirectReports/DEPT/CMO";
- 
-    $.ajax ({
-       url: queryURL,
-       method: "GET",
-     }).then(function(data){
-        console.log(data);
-        data.forEach(function(person){
-            console.log(person.Name + ' ' + person.Title);
+const output = document.querySelector('.output');
+const queryURL= "http://mvc.ad.jocoks.com/OrgDataMVC/DirectReports/DEPT/CMO";
+
+$.ajax ({
+  url: queryURL,
+  method: "GET",
+  }).then(function(data){
+  console.log(data);
+  data.forEach(function(person){
+      console.log(person.Name + ' ' + person.Title);
+
+    $("#alertbtn").click(function(){
+        output.innerHTML += `
+        ${person.Name} ${person.Title}
+        <br>
+        `
         })
-     })
-   
+  })
+
+
+  })
+
+
 });
