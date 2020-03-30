0150ecd8d0d1434fe55326fcf3b2682d151038f5
mhardin <melinda.hardin@jocogov.org>
2020-03-30 16:02:40 -0500

    changed CDN to MDB

--- logic.js
+++ logic.js
@@ -1,8 +1,8 @@
 $(function() {
 
-const outputSrvrs = document.querySelector('.output-srvrs');
 const dropdowncontent_conn = document.querySelector('.dropdown-content_conn');
-const dropdowncontent_svrs = document.querySelector('.dropdown-content_svrs');
+const content_svrs = document.querySelector('.card-content_svrs');
+const pill = document.querySelector('.badge-pill');
 const queryURL= "https://api.myjson.com/bins/19o1hc";
 
 $.ajax ({
@@ -21,12 +21,25 @@ $.ajax ({
 
     data.servers.forEach(info => {
       //console.log(info);
-      $("#btn_svrs").one("click",function(){
-        $('.dropdown-trigger').dropdown();
-        dropdowncontent_svrs.innerHTML += `
-        <li><a href="#!">${info.name}</a></li>
-      `       
-      })
+        content_svrs.innerHTML += `
+        <a class="btn btn-primary" data-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false"
+        aria-controls="multiCollapseExample1">${info.name}</a>
+        <hr>
+
+        <div class="row">
+        
+        <div class="col">
+          <div class="collapse multi-collapse" id="multiCollapseExample1">
+            <div class="card card-body">
+              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil
+              anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+            </div>
+          </div>
+        </div>
+        `
+      
+     
+      // })
     })
   })
 });
