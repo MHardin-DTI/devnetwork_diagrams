3d485fd6ecff59d8b93bab682bbdc9aed35edb99
mhardin <melinda.hardin@jocogov.org>
2020-03-31 14:01:59 -0500

    removed old index

--- logic.js
+++ logic.js
@@ -1,27 +1,27 @@
-$(function() {
+$(function () {
 
-const dropdowncontent_conn = document.querySelector('.dropdown-content_conn');
-const content_svrs = document.querySelector('.card-content_svrs');
-const pill = document.querySelector('.badge-pill');
-const queryURL= "https://api.myjson.com/bins/19o1hc";
+  const dropdowncontent_conn = document.querySelector('.dropdown-content_conn');
+  const content_svrs = document.querySelector('.card-content_svrs');
+  const pill = document.querySelector('.badge-pill');
+  const queryURL = "https://api.myjson.com/bins/19o1hc";
 
-$.ajax ({
-  url: queryURL,
-  method: "GET",
+  $.ajax({
+    url: queryURL,
+    method: "GET",
   }).then(data => {
     console.log(data);
     data.connections.forEach(info => {
-      $("#btn_conn").one("click",function(){
+      $("#btn_conn").one("click", function () {
         $('.dropdown-trigger').dropdown();
         dropdowncontent_conn.innerHTML += `
           <li><a href="#!">${info.name}</a></li>
-        `      
+        `
       })
     })
 
     data.servers.forEach(info => {
       //console.log(info);
-        content_svrs.innerHTML += `
+      content_svrs.innerHTML += `
 
       <div class="accordion" id="accordionExample">
         <div class="card">
@@ -35,14 +35,15 @@ $.ajax ({
       
           <div id="${info.name}" class="collapse hide" aria-labelledby="headingOne" data-parent="#accordionExample">
             <div class="card-body">
+            <h6><strong>Sites </strong></h6> 
             ${info.details}
             </div>
           </div>
         </div>
       </div>
       `
-      
-     
+
+
       // })
     })
   })
